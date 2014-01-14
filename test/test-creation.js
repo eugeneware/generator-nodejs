d1c0b9c3c33a2f87634ddf837d9dd69d9536baa5
Eugene Ware <eugene@noblesamurai.com>
2014-01-14 22:49:29 +1100

    fix tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -35,12 +35,55 @@ describe('nodejs generator', function () {
       'moduleName': 'mymodule',
       'moduleDesc': 'awesome module',
       'keywords': 'something',
+      'useGrunt': true,
+      'testFramework': 'mocha',
+      'assertionLibrary': 'expect.js',
       'githubName': 'octocat',
       'author': 'Octo Cat <main@mail.com>'
     });
 
     this.app.run({}, function () {
-      helpers.assertFiles(expected);
+      expected.forEach(function (file) {
+        if (typeof file === 'string') {
+          helpers.assertFile(file);
+        } else if (Array.isArray(file)) {
+          helpers.assertFileContent(file[0], file[1]);
+        }
+      });
+      done();
+    });
+  });
+
+  it('creates minimal tape files', function (done) {
+    var expected = [
+      'index.js',
+      'test/index.js',
+      '.gitignore',
+      '.travis.yml',
+      ['package.json', /"name": "mymodule"/],
+      'README.md',
+      'LICENSE'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'moduleName': 'mymodule',
+      'moduleDesc': 'awesome module',
+      'keywords': 'something',
+      'useGrunt': false,
+      'testFramework': 'tape',
+      'assertionLibrary': 'none',
+      'githubName': 'octocat',
+      'author': 'Octo Cat <main@mail.com>'
+    });
+
+    this.app.run({}, function () {
+      expected.forEach(function (file) {
+        if (typeof file === 'string') {
+          helpers.assertFile(file);
+        } else if (Array.isArray(file)) {
+          helpers.assertFileContent(file[0], file[1]);
+        }
+      });
       done();
     });
   });
